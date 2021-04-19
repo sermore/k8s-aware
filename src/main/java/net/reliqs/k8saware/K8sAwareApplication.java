44da0a7cc0a699ff0c1c8f4038808c7876f145b5
Sergio Moretti <sergio@reliqs.net>
2021-04-19 02:00:05 +0200

    first commit

---
+++ src/main/java/net/reliqs/k8saware/K8sAwareApplication.java
@@ -0,0 +1,29 @@
+package net.reliqs.k8saware;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableScheduling
+@RefreshScope
+public class K8sAwareApplication {
+
+    @Value("${msg}")
+    private String msg;
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sAwareApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World, please note that " + msg;
+    }
+
+}
---
+++ src/test/java/net/reliqs/k8saware/K8sAwareApplicationTests.java
@@ -0,0 +1,13 @@
+package net.reliqs.k8saware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sAwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
